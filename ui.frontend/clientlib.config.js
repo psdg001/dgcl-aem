90fd8fd26b9e0eec307c10e04d101e070bd07f5b
dhananjay-diriyah <dhananjaya.kumar@publicissapient.com>
2025-07-25 12:57:41 +0530

    Updating the project name from  dgcl-aem to diriyah
    Updating the project name from  dgcl-aem to diriyah

--- ui.frontend/clientlib.config.js
+++ ui.frontend/clientlib.config.js
@@ -26,7 +26,7 @@ const CLIENTLIB_DIR = path.join(
   'content',
   'jcr_root',
   'apps',
-  'dgcl-aem',
+  'diriyah',
   'clientlibs'
 );
 
@@ -45,7 +45,7 @@ module.exports = {
     {
       ...libsBaseConfig,
       name: 'clientlib-dependencies',
-      categories: ['dgcl-aem.dependencies'],
+      categories: ['diriyah.dependencies'],
       assets: {
         // Copy entrypoint scripts and stylesheets into the respective ClientLib
         // directories
@@ -64,8 +64,8 @@ module.exports = {
     {
       ...libsBaseConfig,
       name: 'clientlib-site',
-      categories: ['dgcl-aem.site'],
-      dependencies: ['dgcl-aem.dependencies'],
+      categories: ['diriyah.site'],
+      dependencies: ['diriyah.dependencies'],
       assets: {
         // Copy entrypoint scripts and stylesheets into the respective ClientLib
         // directories
